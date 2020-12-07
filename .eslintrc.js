16fb7799a17a9826fb926b76152036452cf2b3ea
Tony Greeley <tonygreeley@Tonys-iMac.local>
2020-12-07 16:58:18 -0600

    Setup Eslint with Prettier.

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    "prettier"
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module"
+  },
+  rules: {
+    "prettier/prettier": "error"
+  },
+  plugins: ["prettier"]
+};
---
+++ .prettierrc.js
@@ -0,0 +1,6 @@
+module.exports ={
+    semi: true,
+    trailingComma:"none",
+    arrowParens: "avoid",
+    
+}
