9070304012c1499eff9e3a34ac9c99a408c58f4c
Tony Greeley <tonygreeley@Tonys-iMac.local>
2020-12-08 16:35:40 -0600

    Altered linting and husky setup.

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,7 @@ module.exports = {
     commonjs: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-    "prettier"
-  ],
+  extends: ["airbnb-base", "prettier"],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module"
