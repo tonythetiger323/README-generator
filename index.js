5ae5371477dce0d779eacd31ec29459f8362051f
Tony Greeley <tonygreeley@Tonys-iMac.local>
2020-12-08 17:28:30 -0600

    Completed writeToFile function in index.js.

--- index.js
+++ index.js
@@ -5,70 +5,67 @@ const inquirer = require("inquirer");
 
 // array of questions for user
 const questions = [
-    {
-        type: "input",
-        name: "name",
-        message: "What is your name?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is your email address?"
-    },
-    {
-        type: "input",
-        name: "github",
-        message: "What is your GitHub username?"
-    },
-    {
-        type: "input",
-        name: "title",
-        message: "What is your projects name?"
-    },
-    {
-        type: "input",
-        name: "description",
-        message: "Please write a short description of your project."
-    },
-    {
-        type: "list",
-        name: "license",
-        message: "What type of license does your project have?",
-        choices: ["Apache 2.0", "MIT", "GPL 3.0", "BSD 3", "BSD 2", "None"]
-    },
-    {
-        type: "input",
-        name: "install",
-        message: "What command should be run to install dependencies?",
-        default: "npm install"
-    },
-    {
-        type: "input",
-        name: "test",
-        message: "What command should be run to run tests?",
-        default: "npm test"
-    },
-    {
-        type: "input",
-        name: "usage",
-        message: "Please describe how to use the application."
-    },
-    {
-        type: "input",
-        name: "contribute",
-        message: "What are the guidelines for open-source contributions to the repo?"
-    }
+  {
+    type: "input",
+    name: "name",
+    message: "What is your name?"
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?"
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?"
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "What is your projects name?"
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please write a short description of your project."
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What type of license does your project have?",
+    choices: ["Apache 2.0", "MIT", "GPL 3.0", "BSD 3", "BSD 2", "None"]
+  },
+  {
+    type: "input",
+    name: "install",
+    message: "What command should be run to install dependencies?",
+    default: "npm install"
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "What command should be run to run tests?",
+    default: "npm test"
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Please describe how to use the application."
+  },
+  {
+    type: "input",
+    name: "contribute",
+    message:
+      "What are the guidelines for open-source contributions to the repo?"
+  }
 ];
 
 // function to write README file
-const writeToFile = (fileName, data) => {
-
-};
-
+const writeToFile = (fileName, data) =>
+  fs.writeFileSync(path.join(process.cwd(), "output", fileName), data);
 // function to initialize program
-const init = () => {
-
-}
+const init = () => {};
 
 // function call to initialize program
 init();
