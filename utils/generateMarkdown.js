0e523a3760bb7924defc3ebcffee43bee49c8df1
Tony Greeley <tonygreeley@Tonys-iMac.local>
2020-12-09 12:56:48 -0600

    Added function to generate markdown to get a badge for license selected.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,11 @@
+// function to get badge for selected license and render to page
+const licenseBadge = license => {
+    if(license !== "None"){
+        return `![License](https://img.shileds.io/badge/license-${license}-green.svg)`
+    };
+    return "";
+};
+
 // function to generate markdown for READEME
 const generateMarkdown = data => {
     return `# ${data.title}
