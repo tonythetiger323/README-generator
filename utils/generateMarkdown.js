96853e75c088716b0d7c4df705fbe849619ee512
Tony Greeley <tonygreeley@Tonys-iMac.local>
2020-12-09 12:58:42 -0600

    Added comments to code in generateMarkdown.js.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,10 @@
 // function to get badge for selected license and render to page
 const licenseBadge = license => {
+    // If a license is selected return the badge
     if(license !== "None"){
         return `![License](https://img.shileds.io/badge/license-${license}-green.svg)`
     };
+    // If no license is selected, return an empty string
     return "";
 };
 
